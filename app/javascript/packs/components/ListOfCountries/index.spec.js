a9d3389cc8d389f8a18a2dfd1496ed6dc650a5e9
Linielson Rosa <linielsonrosa@gmail.com>
2020-08-23 19:08:26 -0300

    Add tests to ListOfCountry component

---
+++ app/javascript/packs/components/ListOfCountries/index.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import ListOfCountries from './'
+
+describe('<ListOfCountry />', () => {
+  let subject
+
+  function compile() {
+    subject = render(
+      <ListOfCountries>{ <tr><td>Rendered child component</td></tr> }</ListOfCountries>
+    )
+    return subject
+  }
+
+  it('renders the table', () => {
+    compile()
+    expect(subject.container.querySelector('table')).toBeInTheDocument()
+    expect(subject.queryByText('Bandeira')).toBeVisible()
+    expect(subject.queryByText('País')).toBeVisible()
+    expect(subject.queryByText('Sigla')).toBeVisible()
+    expect(subject.queryByText('Moeda')).toBeVisible()
+    expect(subject.queryByText('Blocos Econômicos')).toBeVisible()
+  })
+
+  it('renders the children component', () => {
+    compile()
+    expect(subject.queryByText('Rendered child component')).toBeVisible()
+  })
+})
